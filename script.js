b819a3240ced999161edd242bf8540fdea5938af
esquilo korte <dudu.korte@gmail.com>
2023-08-19 01:06:08 -0300

    Optimized Requests, Warning added, database added
    Less request to game search, TooMany requests warning, offline database added

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const nomeDoJogo = document.getElementById("searchGame");
 
 const useCorsPolicy = true;
 
+function tooManyRequestsWarning(reverse = false, text = "Too Many Request. Please, wait and try again later."){
+    if(reverse){
+        document.getElementById("tooManyRq").textContent = "";
+    }else{
+        console.log("Muitas tentativas seguidas. Por favor, aguarde e tente novamente mais tarde: ");
+        document.getElementById("tooManyRq").textContent = text;
+    }
+}
+
+function verifyImageValid(url, callback){
+    const img = new Image();
+    img.onload = function(){
+        callback(true);
+    }
+    img.onerror = function(){
+        callback(false);
+    }
+    img.src = url;
+}
+
 async function convertUsernameToId(currentid){
     if(!Number.isNaN(Number(steamId))){
         return currentid;
@@ -12,6 +32,13 @@ async function convertUsernameToId(currentid){
         let url;
         useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apiKey}&vanityurl=${username}` : url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apiKey}&vanityurl=${username}`;
         const usernameResponse = await fetch(url);
+        
+        if(usernameResponse.status == 429){
+            tooManyRequestsWarning()
+        }else{
+            tooManyRequestsWarning(true)
+        }
+
         const usernameData = await usernameResponse.json();
         if (usernameData.response.success === 1){
             currentid = usernameData.response.steamid;
@@ -59,15 +86,26 @@ async function searchGameInSteam() {
 
     //Apenas se um nome foi digitado
     if(name){
-        console.log(name)
         //https://cors-anywhere.herokuapp.com/ << usar proxy para contornar política CORS
 
-        //Utilização da API SteamWorks
-        let url;
-        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamApps/GetAppList/v2` : url = `https://api.steampowered.com/ISteamApps/GetAppList/v2`;
-        const response = await fetch(url);
-        const data = await response.json(); //Pega o json com os dados de todos os jogos da steam
-        
+        let data;
+        //Pegando todos os jogos com a api
+        try{
+            let url;
+            useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamApps/GetAppList/v2` : url = `https://api.steampowered.com/ISteamApps/GetAppList/v2`;
+            const response = await fetch(url);
+            if(response.status == 429){
+                tooManyRequestsWarning(false, "Too many requests. Using offline database");
+            }else{
+                tooManyRequestsWarning(true);
+            }
+            data = await response.json(); //Pega o json com os dados de todos os jogos da steam
+        }catch{
+            //Get offline array if API fails
+            const response = await fetch('steam_games.json');
+            data = await response.json();
+        }
+
         //Pegar apenas os jogos dos dados
         const games = data.applist.apps;
         //Transformar nome do jogo pesquisado para lowercase
@@ -85,74 +123,74 @@ async function searchGameInSteam() {
             //pega id do jogo
             const gameId = game.appid;
             //pega a url da imagem do jogo
-            let gameIconURL; 
-            useCorsPolicy ? gameIconURL = `https://cors-anywhere.herokuapp.com/https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009` : gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`;
+            let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`;
             
             //Teste para não ocorrer erro 404 e parar o código
-            const test = await fetch(gameIconURL);
+            //const test = await fetch(gameIconURL);
             //Verifica se a imagem foi recebida com êxito
-            if(test.status == 200){
-                //Adiciona o jogo encontrado na página do site
-                addGameToHTML(gameIconURL, game, containerElement);
-            }else{
-                console.log("URL de jogo inválida")
-            }
+
+            verifyImageValid(gameIconURL, isValid => {
+                if(isValid){
+                    addGameToHTML(gameIconURL, game, containerElement);
+                }else{
+                    console.log('URL de Imagem é inválido');
+                }
+            })
         }
     }
 }
 
 async function searchPlayerById(){
     steamId = htmlsteamId.value;
-
-    try{
-        //Steam id meu próprio para "placeholder" caso a pessoa não coloque nada
-        if(!steamId){
-            steamId = "76561198408735369";;
-        }else{
-            //Verifica se a pessoa digitou um ID ou um nome de usuário
-            steamId = await convertUsernameToId(steamId);
-            console.log(steamId);
-        }
-
-        //Pega as informações do usuário com a api da steam
-        let url;
-        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}` : url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        const playerDetails = data.response.players[0] //Pegar apenas e especificamente os valores do player
-        
-        //Pegar outras informações do player
-        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json` : url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`;
-        const _response = await fetch(url);
-        const _data = await _response.json();
-        const game_count = _data.response.game_count;
-
-        //Usar valores do player
-        console.log("Player Details: ", playerDetails);
-
-        //Atualizando valores do usuário na página do html
-        document.getElementById("playerUsername").textContent = playerDetails.personaname;
-        document.getElementById("userImage").src = playerDetails.avatarfull;
-        document.getElementById("userUrlSteam").href = playerDetails.profileurl;
-        document.getElementById("userUrlSteam").textContent = playerDetails.profileurl;
-        document.getElementById("game-count").textContent = game_count;
-
-
-        //Converter tempo de segundos para data atual da conta criada
-        let timecreated = playerDetails.timecreated;
-        let createdDate = new Date(timecreated * 1000);
-        let formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
-        document.getElementById("userTimeCreated").textContent = formattedDate;
-        //Converter tempo de segundos para data atual da conta deslogada
-        timecreated = playerDetails.lastlogoff;
-        createdDate = new Date(timecreated * 1000);
-        formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
-        document.getElementById("userLastLogoff").textContent = formattedDate;
-
-    }catch(error){
-        //Erro que pode ocorrer caso façam muitas solicitações à api usando id de usuário
-        console.log("Muitas tentativas seguidas. Por favor, aguarde e tente novamente mais tarde: ", error)
+    //Steam id meu próprio para "placeholder" caso a pessoa não coloque nada
+    if(!steamId){
+        steamId = "76561198408735369";;
+    }else{
+        //Verifica se a pessoa digitou um ID ou um nome de usuário
+        steamId = await convertUsernameToId(steamId);
     }
+
+    //Pega as informações do usuário com a api da steam
+    let url;
+    useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}` : url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`;
+    const response = await fetch(url);
+
+    if(response.status == 429){
+        tooManyRequestsWarning()
+    }else{
+        tooManyRequestsWarning(true)
+    }
+
+    const data = await response.json();
+    const playerDetails = data.response.players[0] //Pegar apenas e especificamente os valores do player
+    
+    //Pegar outras informações do player
+    useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json` : url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`;
+    const _response = await fetch(url);
+    const _data = await _response.json();
+    const game_count = _data.response.game_count;
+
+    //Usar valores do player
+    console.log("Player Details: ", playerDetails);
+
+    //Atualizando valores do usuário na página do html
+    document.getElementById("playerUsername").textContent = playerDetails.personaname;
+    document.getElementById("userImage").src = playerDetails.avatarfull;
+    document.getElementById("userUrlSteam").href = playerDetails.profileurl;
+    document.getElementById("userUrlSteam").textContent = playerDetails.profileurl;
+    document.getElementById("game-count").textContent = game_count;
+
+
+    //Converter tempo de segundos para data atual da conta criada
+    let timecreated = playerDetails.timecreated;
+    let createdDate = new Date(timecreated * 1000);
+    let formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
+    document.getElementById("userTimeCreated").textContent = formattedDate;
+    //Converter tempo de segundos para data atual da conta deslogada
+    timecreated = playerDetails.lastlogoff;
+    createdDate = new Date(timecreated * 1000);
+    formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
+    document.getElementById("userLastLogoff").textContent = formattedDate;
 }
 
 async function searchGamesPlayed(){
@@ -180,25 +218,24 @@ async function searchGamesPlayed(){
             //pega Id do jogo
             const gameId = game.appid;
             const playedTimeMin = game.playtime_2weeks; //Usar depois de alguma forma
-            console.log(playedTimeMin, ' : ', game.name);
 
             //pega url da imagm do jogo
             let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`; 
 
-            const test = await fetch(gameIconURL);
-            //Verifica se a imagem foi recebida com êxito
-            if(test.status == 200){
-                addGameToHTML(gameIconURL, game, containerElement);
-            }else{
-                console.log("URL de jogo inválida")
-            }
+            verifyImageValid(gameIconURL, isValid => {
+                if(isValid){
+                    addGameToHTML(gameIconURL, game, containerElement);
+                }else{
+                    console.log('URL de Imagem é inválido');
+                }
+            })
         }
     }catch (error){
         console.log("Houve um erro inesperado, tente novamente mais tarde:", error);
     }
 }
 
-console.log("Steamworks API Site Version 1.2")
+console.log("Steamworks API Site Version 1.3")
 
 document.getElementById("searchGame").addEventListener("focusout", searchGameInSteam);
 document.getElementById("steamId").addEventListener("focusout", searchPlayerById);
