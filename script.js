179960879634482f21980fc9209cd92bd6bc7958
esquilo korte <dudu.korte@gmail.com>
2023-08-18 23:23:04 -0300

    estado gold

--- script.js
+++ script.js
@@ -1,189 +1,195 @@
 const apiKey = 'D21B2F1C42687216FD59600511DF68A7';
-const steamData = document.getElementById("steamData");
-const list = document.getElementById("games-content");
+const htmlsteamId = document.getElementById("steamId");
+const nomeDoJogo = document.getElementById("searchGame");
 
-async function searchGameInSteam(name) {
-    //https://cors-anywhere.herokuapp.com/ << usar proxy para contornar política CORS
+const useCorsPolicy = true;
 
-    //Utilização da API SteamWorks
-    const url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamApps/GetAppList/v2`;
-    const response = await fetch(url);
-    const data = await response.json();
-    
-    //Pegar apenas os jogos dos dados
-    const games = data.applist.apps;
-    //Transformar nome do jogo pesquisado para lowercase
-    const searchTerm = name.toLowerCase();
-
-    //Encontrar Jogo pesquisado na lista
-    const matchingGames = games.filter(game => game.name.toLowerCase().includes(searchTerm));
-
-    return matchingGames;
-
+async function convertUsernameToId(currentid){
+    if(!Number.isNaN(Number(steamId))){
+        return currentid;
+    }else{
+        const username = currentid;
+        let url;
+        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apiKey}&vanityurl=${username}` : url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apiKey}&vanityurl=${username}`;
+        const usernameResponse = await fetch(url);
+        const usernameData = await usernameResponse.json();
+        if (usernameData.response.success === 1){
+            currentid = usernameData.response.steamid;
+            return currentid;
+        }
+    }
 }
 
-async function searchPlayerById(steamId){
+async function addGameToHTML(gameIconURL, game, containerElement){
+    //Adicionando os jogos no html
+    const divElement = document.createElement("div");
+    divElement.classList.add("col-lg-6", "col-md-6", "wow", "fadeInUp");
+    divElement.setAttribute("data-wow-delay", "0s");
+
+    //Criar elemento de card
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("rounded", "shadow", "overflow-hidden", "card-game");
+
+    //Criar outra div
+    const positionElement = document.createElement("div");
+    positionElement.classList.add("position-relative");
+
+    //Criar elemento do link da imagem
+    const url = `https://store.steampowered.com/app/${game.appid}/`
+    const aElement = document.createElement("a");
+    aElement.setAttribute("href", url)
+
+    //Criar elemento da imagem
+    const imgElement = document.createElement("img");
+    imgElement.src = gameIconURL;
+    imgElement.alt = game.name;
+    imgElement.classList.add("card-img-bottom", "card-game-img");
+
+    //Adicionar para cada div
+    aElement.appendChild(imgElement)
+    positionElement.appendChild(aElement);
+    cardElement.appendChild(positionElement);
+    divElement.appendChild(cardElement);
+
+    containerElement.appendChild(divElement);
+}
+
+async function searchGameInSteam() {
+    let name = nomeDoJogo.value;
+
+    //Apenas se um nome foi digitado
+    if(name){
+        console.log(name)
+        //https://cors-anywhere.herokuapp.com/ << usar proxy para contornar política CORS
+
+        //Utilização da API SteamWorks
+        let url;
+        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamApps/GetAppList/v2` : url = `https://api.steampowered.com/ISteamApps/GetAppList/v2`;
+        const response = await fetch(url);
+        const data = await response.json(); //Pega o json com os dados de todos os jogos da steam
+        
+        //Pegar apenas os jogos dos dados
+        const games = data.applist.apps;
+        //Transformar nome do jogo pesquisado para lowercase
+        const searchTerm = name.toLowerCase();
+
+        //Encontrar Jogo pesquisado na lista
+        const matchingGames = games.filter(game => game.name.toLowerCase().includes(searchTerm));
+
+        //Pega elemento do html e limpa
+        const containerElement = document.getElementById("games-container");
+        containerElement.innerHTML = "";
+
+        //Para cada jogo da lista de jogos filtrados na pesquisa
+        for(let game of matchingGames){
+            //pega id do jogo
+            const gameId = game.appid;
+            //pega a url da imagem do jogo
+            let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`; 
+            
+            //Teste para não ocorrer erro 404 e parar o código
+            const test = await fetch(gameIconURL);
+            //Verifica se a imagem foi recebida com êxito
+            if(test.status == 200){
+                //Adiciona o jogo encontrado na página do site
+                addGameToHTML(gameIconURL, game, containerElement);
+            }else{
+                console.log("URL de jogo inválida")
+            }
+        }
+    }
+}
+
+async function searchPlayerById(){
+    steamId = htmlsteamId.value;
+
     try{
+        //Steam id meu próprio para "placeholder" caso a pessoa não coloque nada
         if(!steamId){
             steamId = "76561198408735369";;
+        }else{
+            //Verifica se a pessoa digitou um ID ou um nome de usuário
+            steamId = await convertUsernameToId(steamId);
+            console.log(steamId);
         }
 
-        const url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`
+        //Pega as informações do usuário com a api da steam
+        let url;
+        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}` : url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`;
         const response = await fetch(url);
         const data = await response.json();
-        console.log("Player Details: ", data);
+        const playerDetails = data.response.players[0] //Pegar apenas e especificamente os valores do player
+        
+        //Pegar outras informações do player
+        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json` : url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`;
+        const _response = await fetch(url);
+        const _data = await _response.json();
+        const game_count = _data.response.game_count;
+
+        //Usar valores do player
+        console.log("Player Details: ", playerDetails);
+
+        //Atualizando valores do usuário na página do html
+        document.getElementById("playerUsername").textContent = playerDetails.personaname;
+        document.getElementById("userImage").src = playerDetails.avatarfull;
+        document.getElementById("userUrlSteam").href = playerDetails.profileurl;
+        document.getElementById("userUrlSteam").textContent = playerDetails.profileurl;
+        document.getElementById("game-count").textContent = game_count;
+
+
+        //Converter tempo de segundos para data atual da conta criada
+        let timecreated = playerDetails.timecreated;
+        let createdDate = new Date(timecreated * 1000);
+        let formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
+        document.getElementById("userTimeCreated").textContent = formattedDate;
+        //Converter tempo de segundos para data atual da conta deslogada
+        timecreated = playerDetails.lastlogoff;
+        createdDate = new Date(timecreated * 1000);
+        formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()} ${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`
+        document.getElementById("userLastLogoff").textContent = formattedDate;
+
     }catch(error){
+        //Erro que pode ocorrer caso façam muitas solicitações à api usando id de usuário
         console.log("Muitas tentativas seguidas. Por favor, aguarde e tente novamente mais tarde: ", error)
     }
 }
 
-async function searchGamesPlayed(steamId, debug){
+async function searchGamesPlayed(){
     try{
-        if(debug){
-            games = [
-                {
-                    "appid": 1174180,
-                    "name": "Red Dead Redemption 2",
-                    "playtime_2weeks": 1595,
-                    "playtime_forever": 9202,
-                    "img_icon_url": "5106abd9c1187a97f23295a0ba9470c94804ec6c",
-                    "playtime_windows_forever": 9202,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 1938090,
-                    "name": "Call of Duty®",
-                    "playtime_2weeks": 51,
-                    "playtime_forever": 4960,
-                    "img_icon_url": "8eaf32220060344996cbf11f697a4f4be943e5f3",
-                    "playtime_windows_forever": 4960,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 292030,
-                    "name": "The Witcher 3: Wild Hunt",
-                    "playtime_2weeks": 44,
-                    "playtime_forever": 3032,
-                    "img_icon_url": "6e43cb881e60712c3cccdd7f871baa56acf15328",
-                    "playtime_windows_forever": 3032,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 1670460,
-                    "name": "GameMaker",
-                    "playtime_2weeks": 29,
-                    "playtime_forever": 70,
-                    "img_icon_url": "7e1c283eaa531a6ede26c507ef146a5bb735a2a0",
-                    "playtime_windows_forever": 70,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 646910,
-                    "name": "The Crew 2",
-                    "playtime_2weeks": 18,
-                    "playtime_forever": 1331,
-                    "img_icon_url": "c627b2fa24d61b4042afbd2b183f49b9ca59bc45",
-                    "playtime_windows_forever": 1331,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 10180,
-                    "name": "Call of Duty: Modern Warfare 2 (2009)",
-                    "playtime_2weeks": 9,
-                    "playtime_forever": 1607,
-                    "img_icon_url": "ad502494f1658220f9166c7e17ac90422bf6a479",
-                    "playtime_windows_forever": 1607,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 311210,
-                    "name": "Call of Duty: Black Ops III",
-                    "playtime_2weeks": 2,
-                    "playtime_forever": 5229,
-                    "img_icon_url": "bd3e3a9516b480164df25d16e49ae4ce4a063cb4",
-                    "playtime_windows_forever": 5229,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                },
-                {
-                    "appid": 42690,
-                    "name": "Call of Duty®: Modern Warfare® 3 (2011) - Multiplayer",
-                    "playtime_2weeks": 2,
-                    "playtime_forever": 116,
-                    "img_icon_url": "c3330a875925437d8216949b6571f6e941ba0679",
-                    "playtime_windows_forever": 116,
-                    "playtime_mac_forever": 0,
-                    "playtime_linux_forever": 0
-                }
-            ]
+        steamId = htmlsteamId.value;
 
-            if(!steamId){
-                steamId = "76561198408735369";;
-            }
-
-            console.log("All played games: ", games);
-    
-            //list.innerHTML = "";
-    
-            // for(const game of games){
-            //     const gameId = game.appid;
-            //     const playedTimeMin = game.playtime_2weeks;
-    
-            //     console.log(playedTimeMin, ' : ', game.name);
-    
-    
-            //     //Atualiza lista de jogos recentemente jogados no site
-            //     let listItems = document.createElement("img");
-
-
-
-            //     //Colocar ícone de um jogo
-            //     let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`;
-            //     //https://cdn.akamai.steamstatic.com/steam/apps/1174180/header.jpg?t=1671485009
-    
-            //     //Colocar classe para estilizar
-            //     listItems.className = "list-games";
-            //     listItems.src = gameIconURL;
-            //     list.appendChild(listItems);
-            // }
-
+        if(!steamId){
+            steamId = "76561198408735369";;
         }else{
-            if(!steamId){
-                steamId = "76561198408735369";;
-            }
-    
-            const url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey}&steamid=${steamId}`
-            const response = await fetch(url);
-            const data = await response.json();
-    
-            const games = data.response.games;
-            console.log("All played games: ", games);
-    
-            list.innerHTML = "";
-    
-            for(const game of games){
-                const gameId = game.appid;
-                const playedTimeMin = game.playtime_2weeks;
-    
-                console.log(playedTimeMin, ' : ', game.name);
-    
-    
-                //Atualiza lista de jogos recentemente jogados no site
-                let listItems = document.createElement("div");
-                //Colocar ícone de um jogo
-                let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`;
-                //https://cdn.akamai.steamstatic.com/steam/apps/1174180/header.jpg?t=1671485009
-    
-                //Colocar classe para estilizar
-                //listItems.className = "list-games";
-                listItems.src = gameIconURL;
-                list.appendChild(listItems);
+            steamId = await convertUsernameToId(steamId);
+        }
+
+        let url;
+        useCorsPolicy ? url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey}&steamid=${steamId}` : url = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey}&steamid=${steamId}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const games = data.response.games;
+        console.log("All played games: ", games);
+
+        const containerElement = document.getElementById("user-games-container");
+        containerElement.innerHTML = "";
+
+        for(const game of games){
+            //pega Id do jogo
+            const gameId = game.appid;
+            const playedTimeMin = game.playtime_2weeks; //Usar depois de alguma forma
+            console.log(playedTimeMin, ' : ', game.name);
+
+            //pega url da imagm do jogo
+            let gameIconURL = `https://cdn.akamai.steamstatic.com/steam/apps/${gameId}/header.jpg?t=1671485009`; 
+
+            const test = await fetch(gameIconURL);
+            //Verifica se a imagem foi recebida com êxito
+            if(test.status == 200){
+                addGameToHTML(gameIconURL, game, containerElement);
+            }else{
+                console.log("URL de jogo inválida")
             }
         }
     }catch (error){
@@ -191,21 +197,6 @@ async function searchGamesPlayed(steamId, debug){
     }
 }
 
-function executar_main(){
-    console.clear();
-    
-    //const nomeDoJogo = 'Call of duty'
-    const nomeDoJogo = document.getElementById("searchGame").value;
-    // Chamar a função para buscar jogos com o nome procurado
-    searchGameInSteam(nomeDoJogo).then(result => {
-        console.log('Jogos encontrados:', result);
-    });
-
-    const steamId = document.getElementById("steamId").value;
-    searchPlayerById(steamId);
-    searchGamesPlayed(steamId);
-}
-
-document.getElementById("button").addEventListener("click", executar_main);
-
-searchGamesPlayed(null, true);
+document.getElementById("searchGame").addEventListener("focusout", searchGameInSteam);
+document.getElementById("steamId").addEventListener("focusout", searchPlayerById);
+document.getElementById("steamId").addEventListener("focusout", searchGamesPlayed);
